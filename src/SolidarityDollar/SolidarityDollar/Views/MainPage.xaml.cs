058562d6a713385d9b396a29e64e7987fb56a4bc
Pablo Piovano <ppiova@gmail.com>
2020-08-18 15:22:23 -0300

    Added validation Entry Value and Entry mode = twoway and calculated rates when type in entry

--- src/SolidarityDollar/SolidarityDollar/Views/MainPage.xaml.cs
+++ src/SolidarityDollar/SolidarityDollar/Views/MainPage.xaml.cs
@@ -23,49 +23,6 @@ namespace SolidarityDollar.Views
         }
 
 
-
-
-        private async void RateTextEntry_Completed(System.Object sender, System.EventArgs e)
-        {
-            if (await ValidarFormulario())
-            {
-                //GoodJob
-            }
-               //Dont Pass      
-
-
-        }
-        private async Task<bool> ValidarFormulario()
-        {
-            //Valida si el valor en el Entry se encuentra vacio o es igual a Null
-            if (String.IsNullOrWhiteSpace(RateTextEntry.Text))
-            {
-                await this.DisplayAlert("Advertencia", "Debe ingresar un monto", "OK");
-                return false;
-            }
-            //Valida que solo se ingresen números
-            else if (!RateTextEntry.Text.ToCharArray().All(Char.IsNumber))
-            {
-                await this.DisplayAlert("Advertencia", "Ingrese solo números, favor de validar.", "OK");
-                return false;
-            }
-            //else
-            //{
-            //    //Valida si se ingresan caracteres especiales
-            //    string caractEspecial = @"^[^ ][a-zA-Z ]+[^ ]$";
-            //    bool resultado = Regex.IsMatch(RateTextEntry.Text, caractEspecial, RegexOptions.IgnoreCase);
-            //    if (!resultado)
-            //    {
-            //        await this.DisplayAlert("Advertencia", "No se aceptan caracteres especiales, intente de nuevo.", "OK");
-            //        return false;
-            //    }
-            //}
-
-          
-            return true;
-        }
-
-
         private async void OnTapGestureRecognizerTapped(object sender, EventArgs args)
         {
             if (!string.IsNullOrWhiteSpace(RateTextEntry.Text))
