8d22b79735b5549cd9d650e07e796d5e8e11bc1d
Pablo Piovano <ppiova@gmail.com>
2020-09-08 12:04:18 -0300

    AppConstants
    AppConstants

--- src/SolidarityDollar/SolidarityDollar.iOS/appcenter-pre-build.sh
+++ src/SolidarityDollar/SolidarityDollar.iOS/appcenter-pre-build.sh
@@ -1,23 +1,3 @@
-#!/usr/bin/env bash
-#
-# For Xamarin, change some constants located in some class of the app.
-# In this sample, suppose we have an AppConstant.cs class in shared folder with follow content:
-#
-# namespace Core
-# {
-#     public class AppConstant
-#     {
-#         public const string ApiUrl = "https://CMS_MyApp-Eur01.com/api";
-#     }
-# }
-# 
-# Suppose in our project exists two branches: master and develop. 
-# We can release app for production API in master branch and app for test API in develop branch. 
-# We just need configure this behaviour with environment variable in each branch :)
-# 
-# The same thing can be perform with any class of the app.
-#
-# AN IMPORTANT THING: FOR THIS SAMPLE YOU NEED DECLARE API_URL ENVIRONMENT VARIABLE IN APP CENTER BUILD CONFIGURATION.
 
 if [ -z "$API_URL" ]
 then
@@ -43,18 +23,18 @@ then
     exit
 fi
 
-APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/SolidarityDollar/SolidarityDollar/AppConstant.cs
+APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/SolidarityDollar/SolidarityDollar/AppConstants.cs
 
 if [ -e "$APP_CONSTANT_FILE" ]
 then
-    echo "Updating ApiUrl to $API_URL in AppConstant.cs"
+    echo "Updating ApiUrl to $API_URL in AppConstants.cs"
     sed -i '' 's#ApiUrl = "[-A-Za-z0-9:_./]*"#ApiUrl = "'$API_URL'"#' $APP_CONSTANT_FILE
-    echo "Updating ApiKeyDolarSolidario to $APP_SECRETS in AppConstant.cs"
+    echo "Updating ApiKeyDolarSolidario to $APP_SECRETS in AppConstants.cs"
     sed -i '' 's#ApiKeyDolarSolidario = "[-A-Za-z0-9:_./]*"#ApiKeyDolarSolidario = "'$APP_SECRETS'"#' $APP_CONSTANT_FILE
     
-    echo "Updating AppCenterKeyiOS to $APP_CENTERIOS in AppConstant.cs"
+    echo "Updating AppCenterKeyiOS to $APP_CENTERIOS in AppConstants.cs"
     sed -i '' 's#AppCenterKeyiOS = "[-A-Za-z0-9:_./]*"#AppCenterKeyiOS = "'$APP_CENTERIOS'"#' $APP_CONSTANT_FILE
-     echo "Updating AppCenterKeyAndroid to $APP_CENTERANDROID in AppConstant.cs"
+     echo "Updating AppCenterKeyAndroid to $APP_CENTERANDROID in AppConstants.cs"
     sed -i '' 's#AppCenterKeyAndroid = "[-A-Za-z0-9:_./]*"#AppCenterKeyAndroid = "'$APP_CENTERANDROID'"#' $APP_CONSTANT_FILE
   
     
