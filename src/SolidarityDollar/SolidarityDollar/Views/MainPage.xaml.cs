6d7af658622f49bb546d992d2d84b047451e3844
Pablo Piovano <ppiova@gmail.com>
2020-09-08 02:49:04 -0300

    Little changes
    Little changes

--- src/SolidarityDollar/SolidarityDollar/Views/MainPage.xaml.cs
+++ src/SolidarityDollar/SolidarityDollar/Views/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using SolidarityDollar.ViewModel;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -26,12 +27,21 @@ namespace SolidarityDollar.Views
 
         private async void OnTapGestureRecognizerTapped(object sender, EventArgs args)
         {
-            var analyticsData = new Dictionary<string, string> { { "Value", RateTextEntry.Text } };
-            Analytics.TrackEvent("Speak Value", analyticsData);
+            try
+            {
+                var analyticsData = new Dictionary<string, string> { { "Value", RateTextEntry.Text } };
+                Analytics.TrackEvent("Speak Value", analyticsData);
 
-            if (!string.IsNullOrWhiteSpace(RateTextEntry.Text))
-              
-            await TextToSpeech.SpeakAsync(RateTextEntry.Text);
+                if (!string.IsNullOrWhiteSpace(RateTextEntry.Text))
+
+                    await TextToSpeech.SpeakAsync(RateTextEntry.Text);
+            }
+            catch (Exception e)
+            {
+                Crashes.TrackError(e);
+                throw;
+            }
+           
            
         }
 
