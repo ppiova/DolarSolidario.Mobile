fbc2cafcf2ed2ac09d888f5f35f99ee1fa0fb0d3
Pablo Piovano <ppiova@gmail.com>
2020-09-09 15:29:10 -0300

    minor tweaks
    Changes in the info culture and in the label icon app

--- src/SolidarityDollar/SolidarityDollar/AppConstant.cs
+++ src/SolidarityDollar/SolidarityDollar/AppConstant.cs
@@ -4,7 +4,7 @@ namespace SolidarityDollar
     public static class AppConstant
     {
         //API Keys
-        public const string ApiKeyDolarSolidario = "";
+        public const string ApiKeyDolarSolidario = "1d32548e20454b8c87da51c561861016";
         //EndPoints
         public const string ApiUrl = "https://dolarsolidarioapimanagement.azure-api.net";
         public const string ControllerLastRate = "/api/rates/GetLastRate";
